a03369005de0ad3be3da21db7c0ab2fc1819f734
Mick Hill - MacBook Pro M1 <mickhill@hotmail.com.br>
2024-11-26 20:46:20 -0300

    base do cucumber

---
+++ src/test/java/steps/PesquisaGoogleSteps.java
@@ -0,0 +1,23 @@
+package steps;
+
+import io.cucumber.java.pt.Dado;
+import io.cucumber.java.pt.Entao;
+import io.cucumber.java.pt.Quando;
+
+public class PesquisaGoogleSteps
+{
+    @Dado("que o usuario esta na pagina do google")
+    public void que_o_usuario_esta_na_pagina_do_google()
+    {
+    }
+
+    @Quando("o usuario prencher o campo de pesquisa com {string}")
+    public void o_usuario_prencher_o_campo_de_pesquisa_com(String string)
+    {
+    }
+
+    @Entao("a pagina me redirecionara para os resultados da pesquisa")
+    public void a_pagina_me_redirecionara_para_os_resultados_da_pesquisa()
+    {
+    }
+}
---
+++ src/test/resources/pesquisa-google.feature
@@ -0,0 +1,8 @@
+# language:pt
+Funcionalidade: Pesquisa Google
+
+  @mick_lindo
+  Cenario: Pesquisa com Sucesso
+    Dado que o usuario esta na pagina do google
+    Quando o usuario prencher o campo de pesquisa com "Mick Lindo"
+    Entao a pagina me redirecionara para os resultados da pesquisa
