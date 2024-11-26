d4f817756f13c51fd7caa26accf2802aa933d881
Mick Hill - MacBook Pro M1 <mickhill@hotmail.com.br>
2024-11-25 21:10:39 -0300

    add PageMapping

--- src/test/java/basic/PageMapping.java
+++ src/test/java/basic/PageMapping.java
@@ -1,11 +1,13 @@
 package basic;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.SearchContext;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class PageMapping
 {
-    public static void main(String[] args)
+    public static void main(String[] args) throws InterruptedException
     {
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.bmw.com.br/pt/index.html");
@@ -23,8 +25,16 @@ public class PageMapping
          *  + linkText
          *
          * */
+        Thread.sleep(3000);
+        SearchContext contexto = driver.findElement(By.cssSelector("#contentpage-79b5757632 > epaas-consent-drawer-shell")).getShadowRoot();
+        contexto.findElement(By.cssSelector("button.accept-button.button-primary")).click();
 
+        driver.findElement(By.cssSelector("nav ul > li:nth-child(4) > a")).click();
+        Thread.sleep(1500);
+        driver.findElement(By.id("aems-inputsearch")).sendKeys("M3 GTR");
+        driver.findElement(By.id("aems-buttonsearch")).click();
 
+        Thread.sleep(5000);
         driver.quit();
     }
 }
