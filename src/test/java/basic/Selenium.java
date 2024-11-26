14016710a55467de8ec426520191b16a8a46b9dd
Mick Hill - MacBook Pro M1 <mickhill@hotmail.com.br>
2024-11-25 21:21:23 -0300

    add junit

--- src/test/java/basic/Selenium.java
+++ src/test/java/basic/Selenium.java
@@ -5,13 +5,13 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Selenium
 {
-    /**
-     * Navegador   - Google Chrome
-     * Selenium    - selenium-java com [WebDriver Embutido]
-     * Java        - JDK 11
-     * */
     public static void main(String[] args) throws InterruptedException
     {
+        /**
+         * Navegador   - Google Chrome
+         * Selenium    - selenium-java com [WebDriver Embutido]
+         * Java        - JDK 11
+         * */
         WebDriver driver = new ChromeDriver();
 
         driver.get("https://google.com.br");
