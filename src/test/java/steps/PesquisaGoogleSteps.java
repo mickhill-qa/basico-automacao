d006ff5543fd7994f1b7e814f319dff0e005b3d1
Mick Hill - MacBook Pro M1 <mickhill@hotmail.com.br>
2024-11-26 21:19:33 -0300

    Teste com Cucumber, JUnit Selenium e Java

--- src/test/java/steps/PesquisaGoogleSteps.java
+++ src/test/java/steps/PesquisaGoogleSteps.java
@@ -1,23 +1,60 @@
 package steps;
 
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
 import io.cucumber.java.pt.Dado;
 import io.cucumber.java.pt.Entao;
 import io.cucumber.java.pt.Quando;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class PesquisaGoogleSteps
 {
+    WebDriver driver;
+
+    /**
+     * Hooks
+     * */
+    @Before
+    public void antesDoTeste()
+    {
+        driver = new ChromeDriver();
+    }
+
+    @After
+    public void depoisDoTeste()
+    {
+        driver.quit();
+    }
+
+    /**
+     * Steps of Tests
+     * */
     @Dado("que o usuario esta na pagina do google")
     public void que_o_usuario_esta_na_pagina_do_google()
     {
+        driver.get("https://google.com.br");
     }
 
     @Quando("o usuario prencher o campo de pesquisa com {string}")
-    public void o_usuario_prencher_o_campo_de_pesquisa_com(String string)
+    public void o_usuario_prencher_o_campo_de_pesquisa_com(String texto_pesquisa)
     {
+        driver.findElement(By.id("APjFqb")).sendKeys(texto_pesquisa);
+        driver.findElement(By.xpath("(//input[@name='btnK'])[2]")).click();
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Entao("a pagina me redirecionara para os resultados da pesquisa")
     public void a_pagina_me_redirecionara_para_os_resultados_da_pesquisa()
     {
+        String urlEsperada = "https://www.google.com.br/search?q=";
+        String urlDaTela = driver.getCurrentUrl();
+        Assert.assertTrue(urlDaTela.contains(urlEsperada));
     }
 }
--- src/test/resources/pesquisa-google.feature
+++ src/test/resources/pesquisa-google.feature
@@ -4,5 +4,5 @@ Funcionalidade: Pesquisa Google
   @mick_lindo
   Cenario: Pesquisa com Sucesso
     Dado que o usuario esta na pagina do google
-    Quando o usuario prencher o campo de pesquisa com "Mick Lindo"
+    Quando o usuario prencher o campo de pesquisa com "teixeirinha"
     Entao a pagina me redirecionara para os resultados da pesquisa
